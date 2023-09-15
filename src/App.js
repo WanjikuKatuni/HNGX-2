5ba044ef4ea57fcd1f88ea48db0e7a96758a639d
wanjiku <annritawanjiku77@gmail.com>
2023-09-15 13:12:28 +0300

    added footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Featured } from "./Components/Featured";
+import { Footer } from "./Components/Footer";
 import { Landing } from "./Components/Landing";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <Landing/>
       <Featured/>
+      <Footer/>
     </div>
   );
 }
