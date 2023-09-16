0cf28f9432258382ee3a276af0943384999f5436
wanjiku <annritawanjiku77@gmail.com>
2023-09-16 12:59:30 +0300

    updated footer with reacticons

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import {FaFacebookSquare, FaInstagram, FaTwitter, FaYoutube} from 'react-icons/fa'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <div className="footer-info pt-40 pb-9 flex flex-col items-center">
+        <div className="icons flex space-x-10 items-center mb-10">
+          <FaFacebookSquare/>
+          <FaInstagram />
+          <FaTwitter />
+          <FaYoutube />
+
+        </div>
+        <div className="footer-info flex space-x-5 items-center mb-10">
+          <p>Conditions of Use</p>
+          <p>Privacy & Policy</p>
+          <p>Press Room</p>
+        </div>
+        <div className="copyright details flex space-x-2 items-center mb-10">
+          <p>2021</p>
+          <p>Movie Box by Ciku</p>
+        </div>
+      </div>
+    </>
   )
 }
